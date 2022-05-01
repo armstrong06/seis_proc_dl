d96faec3716d035cc84b1f3ea97900a99ed3e205
Alysha Armstrong <aa.armstrong6@gmail.com>
2022-04-30 22:54:05 -0600

    Getting 1C UUSS data

--- data_processing/call_gatherdata_3c.py
+++ data_processing/call_gatherdata_3c.py
@@ -10,18 +10,27 @@ archive_dir = '/uufs/chpc.utah.edu/common/home/koper-group1/bbaker/waveformArchi
 catalog_dir = '/uufs/chpc.utah.edu/common/home/koper-group1/bbaker/waveformArchive/data/'
 output_dir = '/uufs/chpc.utah.edu/common/home/koper-group1/alysha/Yellowstone/data/waveformArchive/uuss2021'
 phase_type = "P"
+n_comps = 1
 waveform_halfwidth = 10
 
 processing_function = uuss.ThreeComponentPicker.ZRUNet.ProcessData()
 
 # Initialize 
 gatherer = ThreeComponentGatherer(archive_dir, processing_function)
-infilename_roots = ['currentEarthquakeArrivalInformation', 'currentBlastArrivalInformation', 'historicalEarthquakeArrivalInformation']
-outfilename_roots = ['current_earthquake_catalog', 'current_blast_catalog', 'historical_earthquake_catalog']
-for infilename_root, event_type, outfilename_root in zip(infilename_roots, ['le', 'qb', 'le'], outfilename_roots):
+
+infilename_roots = ['currentEarthquakeArrivalInformation','historicalEarthquakeArrivalInformation']
+outfilename_roots = ['current_earthquake_catalog', 'historical_earthquake_catalog']
+event_types = ["le", "le"]
+
+if phase_type == "P":
+    infilename_roots.append('currentBlastArrivalInformation')
+    outfilename_roots.append('current_blast_catalog')
+    event_types.append("qb")
+
+for infilename_root, event_type, outfilename_root in zip(infilename_roots, event_types, outfilename_roots):
     print(f'loading {outfilename_root}...')
-    filename = os.path.join(catalog_dir, f"{infilename_root}3C.csv")
-    outfilename_root = os.path.join(output_dir, f"{phase_type}_{outfilename_root}")
+    filename = os.path.join(catalog_dir, f"{infilename_root}{n_comps}C.csv")
+    outfilename_root = os.path.join(output_dir, f"{phase_type}_{outfilename_root}_{n_comps}c")
     gatherer.process_and_save_waveforms(filename, phase_type, outfilename_root, event_type, waveform_halfwidth)
     
 gatherer.close()
--- detectors/data_processing/call_split_data_s_uuss.py
+++ detectors/data_processing/call_split_data_s_uuss.py
@@ -1,33 +1,25 @@
 import sys
-sys.path.append('..')
-from data_processing import split_data
-import os
+sys.path.append("/uufs/chpc.utah.edu/common/home/u1072028/PycharmProjects/seis-proc-dl")
+from data_processing.split_data_detectors import SplitDetectorData
+import numpy as np
+import pandas as pd
+from utils.file_manager import Write
 
-#np.random.seed(49230)
 window_duration = 10.0
-n_duplicate_s_train = 2
+n_duplicate_train = 2
 dt = 0.01  # Sampling period (seconds)
 train_frac = 0.8
 noise_train_frac = 0.8
 test_frac = 0.5
+max_pick_shift = 250
 
-pref = '/uufs/chpc.utah.edu/common/home/koper-group1/alysha/Yellowstone/data'
-wf_pref = f'{pref}/waveformArchive/sDetector'
-h5_file_name = wf_pref + '/uuss_current_earthquake_catalog_3C.h5'
-meta_csv_file = wf_pref + '/uuss_current_earthquake_catalog_3C.csv'
-noise_h5_file_name = [f'{pref}/yellowstone/allNoiseYellowstoneWaveforms.h5',
-                      f'{pref}/magna2020_srl/combined_signal/allnoiseS.10s.2dup.h5']
+pref = '/uufs/chpc.utah.edu/common/home/koper-group1/alysha/Yellowstone/data/waveformArchive'
+ys_noise_h5_filename = f'{pref}/noise/allNoiseYellowstoneWaveforms.h5'
+magna_noise_h5_filename = f'{pref}/noise/allNoiseMagnaWaveforms.P.10s.h5'
 
-outpref = f'{wf_pref}/uuss_resampled_10s'
-train_python_file_name = '%s/trainS.%ds.%ddup.h5' % (outpref, int(window_duration), n_duplicate_s_train)
-validate_python_file_name = '%s/validateS.%ds.%ddup.h5' % (outpref, int(window_duration), n_duplicate_s_train)
-test_python_file_name = '%s/testS.%ds.%ddup.h5' % (outpref, int(window_duration), n_duplicate_s_train)
-validate_df_name = '%s/validateS.%ds.%ddup.df.csv' % (outpref, int(window_duration), n_duplicate_s_train)
-test_df_name = '%s/testS.%ds.%ddup.df.csv' % (outpref, int(window_duration), n_duplicate_s_train)
-train_df_name = '%s/trainS.%ds.%ddup.df.csv' % (outpref, int(window_duration), n_duplicate_s_train)
+pref = '/uufs/chpc.utah.edu/common/home/koper-group1/alysha/Yellowstone/data/waveformArchive/uuss2021'
 
-# For NGB events
-NGB_outfile_root = '%s/ngbS.%ds.%ddup' % (outpref, int(window_duration), n_duplicate_s_train)
+# For NGB events - Don't need these for STEAD data
 # Remove events within these bounds
 lat_min = 44.680
 lat_max = 44.870
@@ -48,26 +40,63 @@ bounds = {"lat_min": lat_min,
           "date_min": date_min,
           "date_max": date_max}
 
-extract_events_params = {"bounds":bounds, "outfile_root":NGB_outfile_root}
+extract_events_params = {"bounds":bounds, "name":"NGB"}
 
-if not os.path.exists(outpref):
-    os.makedirs(outpref)
+## Current Earthquakes
+h5_filename = f'{pref}/S_current_earthquake_catalog.h5'
+meta_file = f'{pref}/S_current_earthquake_catalog.csv'
+outpref = f"{pref}/S_resampled_10s/currenteq."
 
-split_data.combine(meta_csv_file,
-                   h5_file_name,  # name of archive with the picked waveforms
-                   noise_h5_file_name,  # name of archive with the noise waveforms
-                   train_python_file_name,  # name of the training file
-                   validate_python_file_name,
-                   test_python_file_name,  # name of the test file
-                   validate_df_name,  # name of the csv file with information on the validation rows
-                   test_df_name,  # name of the csv file with information on the test rows
-                   train_frac=train_frac,  # e.g., keep 80 pct of signals for training and 0.2 pct for validation
-                   test_frac=test_frac,  # e.g., keep 100 pct of remaining 20 pct of data for testing as opposed to validation
-                   noise_train_frac=noise_train_frac,  # e.g., keep 80 pct of noise for training and 0.2 pct for validation
-                   n_duplicate_train=n_duplicate_s_train,
-                   # randomly repeat waveforms in training dataset but randomize starting location
-                   train_df_name=train_df_name,  # Name of csv file with information on training rows - I added this,
-                   window_duration=window_duration,
-                   dt=dt,
-                   noise_meta_file=None,
-                   extract_events_params=extract_events_params)
+spliter = SplitDetectorData(window_duration, dt, max_pick_shift, n_duplicate_train, outpref)
+spliter.load_signal_data(h5_filename, meta_file)
+spliter.split_signal(train_frac, test_frac, extract_events_params=extract_events_params)
+spliter.process_signal()
+
+spliter.load_noise_data([ys_noise_h5_filename, magna_noise_h5_filename])
+spliter.split_noise(noise_train_frac, test_frac)
+spliter.process_noise()
+
+spliter.write_combined_datasets()
+ceq_train,ceq_test,ceq_validate = spliter.return_signal()
+ceq_train_df, ceq_test_df, ceq_validate_df = spliter.return_signal_meta()
+ceq_train_noise, ceq_test_noise, ceq_validate_noise = spliter.return_noise()
+ceq_train_noise_df, ceq_test_noise_df, ceq_validate_noise_df = spliter.return_noise_meta()
+
+# Historical Earthquakes
+h5_filename = f'{pref}/S_historical_earthquake_catalog.h5'
+meta_file = f'{pref}/S_historical_earthquake_catalog.csv'
+outpref = f"{pref}/S_resampled_10s/combined."
+spliter = SplitDetectorData(window_duration, dt, max_pick_shift, 1, outfile_pref=outpref)
+spliter.load_signal_data(h5_filename, meta_file, min_training_quality=0.75)
+spliter.split_signal(0.2, 0.98, extract_events_params=None)
+spliter.process_signal(boxcar_widths={0: 31, 1: 51, 2: 71})
+heq_train,heq_test,heq_validate = spliter.return_signal()
+heq_train_df, heq_test_df, heq_validate_df = spliter.return_signal_meta()
+
+def concate_splits(ceq_split, ceq_split_df, heq_split, heq_split_df, noise_split=None, noise_split_df=None):
+    X = np.concatenate([ceq_split[0], heq_split[0]])
+    y = np.concatenate([ceq_split[1], heq_split[1]])
+    T = np.concatenate([ceq_split[2], heq_split[2]])
+    df = pd.concat([ceq_split_df, heq_split_df])
+    if noise_split is not None:
+        X = np.concatenate([X, noise_split[0]])
+        y = np.concatenate([y, noise_split[1]])
+        T = np.concatenate([T, noise_split[2]])
+    if noise_split_df is not None:
+        df = pd.concat([df, noise_split_df])
+
+    return [X, y, T], df
+
+train_data, train_df = concate_splits(ceq_train, ceq_train_df,heq_train, heq_train_df, ceq_train_noise)
+
+test_data, test_df = concate_splits(ceq_test, ceq_test_df,heq_test, heq_test_df, ceq_test_noise)
+
+validate_data, validate_df = concate_splits(ceq_validate, ceq_validate_df, heq_validate, heq_validate_df, ceq_validate_noise)
+
+Write.h5py_file(["X", "Y", "Pick_index"], train_data, spliter.make_filename("train", "h5"))
+Write.h5py_file(["X", "Y", "Pick_index"], test_data, spliter.make_filename("test", "h5"))
+Write.h5py_file(["X", "Y", "Pick_index"], validate_data, spliter.make_filename("validate", "h5"))
+
+train_df.to_csv(spliter.make_filename("train", "csv"), index=False)
+test_df.to_csv(spliter.make_filename("test", "csv"), index=False)
+validate_df.to_csv(spliter.make_filename("validate", "csv"), index=False)
