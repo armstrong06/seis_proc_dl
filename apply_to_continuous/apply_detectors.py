46512e0b2f4e9fff4d829c63e95e6ba40938d2f3
Alysha Armstrong <aa.armstrong6@gmail.com>
2023-09-25 11:03:24 -0600

    Make DataLoader class

--- apply_to_continuous/apply_detectors.py
+++ apply_to_continuous/apply_detectors.py
@@ -2,105 +2,154 @@ import obspy
 from obspy.core.utcdatetime import UTCDateTime as UTC
 import numpy as np
 
-def get_n_windows(npts, window_length, sliding_interval):
-    return (npts-window_length)//sliding_interval + 1
+class DataLoader():
+    def __init__(self) -> None:
+        self.continuous_data = None
+        self.meta_data = None
+        self.gaps = None
 
-def load_channel_data(file, min_signal_percent=1, expected_file_duration_s=86400):
-    """Reads in a miniseed file and check for gaps. Gaps are interpolated. 
+    def load_3c_data(self, fileE, fileN, fileZ):
+        st_E, gaps_E = self.load_channel_data(fileE)
+        st_N, gaps_N = self.load_channel_data(fileN)
+        st_Z, gaps_Z = self.load_channel_data(fileZ)
 
-    Args:
-        file (str): Path to the miniseed file to read in.
-        min_signal_percent (int, optional): Minimum percent signal required to process the file. Defaults to 1.
-        max_duration_s (int, optional): Expected duration of the miniseed file in seconds. Defaults to 86400.
+        # If one of the channels was skipped, return the entire day as a gap
+        if st_E is None:
+            self.continuous_data = None
+            self.gaps = gaps_E
+        elif st_N is None:
+            self.continuous_data = None
+            self.gaps = gaps_N
+        elif st_Z is None:
+            self.continuous_data = None
+            self.gaps = gaps_Z
 
-    Returns:
-       tupple: (Obspy Stream, list of gaps)
-    """ 
+        starttimes = [st_E[0].stats.starttime, st_N[0].stats.starttime, st_Z[0].stats.starttime]
+        endtimes = [st_E[0].stats.endtime, st_N[0].stats.endtime, st_Z[0].stats.endtime]
+        npts = [st_E[0].stats.npts, st_N[0].stats.npts, st_Z[0].stats.npts]
 
-    # use obspy to load the miniseed file(s)
-    st = obspy.read(file)
+        assert abs(st_E[0].stats.starttime - st_N[0].stats.starttime) < st_N.stats.dt
+        assert abs(st_N[0].stats.starttime - st_Z[0].stats.starttime) < st_N.stats.dt
+        assert abs(st_E[0].stats.endtime - st_N[0].stats.endtime) < st_N.stats.dt
+        assert abs(st_N[0].stats.endtime - st_Z[0].stats.endtime) < st_N.stats.dt
+        assert st_E[0].stats.npts == st_N[0].stats.npts and st_N[0].stats.npts == st_Z[0].stats.npts
 
-    sampling_rate = round(st[0].stats.sampling_rate)
-    # TODO: this only works for days, not hours - fine for me
-    # TODO: If the startime happens to be on the day before, it'll mess this up
-    starttime = st[0].stats.starttime
-    desired_start = UTC(starttime.year, starttime.month, starttime.day)
-    desired_end = desired_start + expected_file_duration_s
+        assert len(np.unique(starttimes)) == 1
+        assert len(np.unique(endtimes)) == 1
+        assert len(np.unique(npts)) == 1
 
-    # If there is not enough signal in this day, skip the day
-    total_npts = np.sum([st[i].stats.npts for i in range(len(st))])
-    max_npts = expected_file_duration_s*round(sampling_rate)
-    if (total_npts/max_npts)*100 < min_signal_percent:
-        # Return the entire file period as a gap
-        return None, [format_edge_gaps(st, desired_start, desired_end, entire_file=True)]
+        cont_data = np.zeros((npts[0], 3))
+        cont_data[:, 0] = st_E[0].data
+        cont_data[:, 1] = st_N[0].data
+        cont_data[:, 2] = st_Z[0].data
 
-    # Save gaps so I know to ignore any detections in that region later
-    # Don't save gaps if they are smaller than 5 samples for 100 Hz, 2 samples for 40 Hz
-    gaps = st.get_gaps(min_gap=0.05)
-    # Still need to interpolate the gaps if they are less than min_gap
-    if len(st.get_gaps()) > 0:
-        # Sometimes the gaps have different sampling rates and delta values, not totally sure why. 
-        # The sampling rates do not equal npts/duration for the gap either.
-        try:
-            st.merge(fill_value='interpolate')
-        except:
-            delta = round(1/sampling_rate, 3)
+        gaps = gaps_E + gaps_N + gaps_Z
 
-            for tr in st:
-                tr.stats.sampling_rate = sampling_rate
-                tr.stats.delta = delta
+        self.continuous_data = cont_data
+        self.gaps = gaps
+        # TODO: Update this
+        self.meta_data = st_E[0].stats
 
-            st.merge(fill_value='interpolate')
+    def load_channel_data(self, file, min_signal_percent=1, expected_file_duration_s=86400):
+        """Reads in a miniseed file and check for gaps. Gaps are interpolated. 
 
-    # Make sure the trace is the desired length (fill in ends if needed) TODO: is it possible that the files are too long?
-    # Check for gaps at the start/end of the day and save if they exist
-    start_gap, end_gap = format_edge_gaps(st, desired_start, desired_end)
-    if start_gap is not None:
-        gaps.insert(0, start_gap)
-        # Fill the start of the trace with the first value, if needed
-        st = st.trim(starttime=desired_start, pad=True, fill_value=st[0].data[0])
-    if end_gap is not None:
-        gaps += [end_gap]
-        # Fill the end of the trace with the last value, if needed
-        st = st.trim(endtime=desired_end, pad=True, fill_value=st[0].data[-1])
+        Args:
+            file (str): Path to the miniseed file to read in.
+            min_signal_percent (int, optional): Minimum percent signal required to process the file. Defaults to 1.
+            max_duration_s (int, optional): Expected duration of the miniseed file in seconds. Defaults to 86400.
 
-    return st, gaps 
+        Returns:
+        tupple: (Obspy Stream, list of gaps)
+        """ 
 
-def format_edge_gaps(st, desired_start, desired_end, entire_file=False):
-    """Checks for gaps greater than 1 second at the start and end of an Obspy Stream.
-    Only looks at the first trace in the stream. *The desired endtime may be off by ~1 second*
-    If a gap exists returns a list in the format of Obspy's get_gaps(), otherwise returns None.
+        # use obspy to load the miniseed file(s)
+        st = obspy.read(file)
 
-    Args:
-        st (Obspy Stream): stream to check for gaps
-        max_duration_s (int): expected duration of the file in seconds
-        entire_file (boolean, optional): True if the gap is the entire file. Defaults to False.
+        sampling_rate = round(st[0].stats.sampling_rate)
+        # TODO: this only works for days, not hours - fine for me
+        # TODO: If the startime happens to be on the day before, it'll mess this up
+        starttime = st[0].stats.starttime
+        desired_start = UTC(starttime.year, starttime.month, starttime.day)
+        desired_end = desired_start + expected_file_duration_s
 
-    Returns:
-        tupple or list: tupple of gap info lists for the beginning and end of the trace or a list for the entire file gap. 
-    """
-    starttime = st[0].stats.starttime
-    endtime = st[0].stats.endtime 
-    sampling_rate = st[0].stats.sampling_rate
+        # If there is not enough signal in this day, skip the day
+        total_npts = np.sum([st[i].stats.npts for i in range(len(st))])
+        max_npts = expected_file_duration_s*round(sampling_rate)
+        if (total_npts/max_npts)*100 < min_signal_percent:
+            # Return the entire file period as a gap
+            return None, [self.format_edge_gaps(st, desired_start, desired_end, entire_file=True)]
 
-    if entire_file:
-        max_duration_s = desired_end - desired_start
-        return [st[0].stats.network, st[0].stats.station, st[0].stats.location, st[0].stats.channel, 
-                     desired_start, desired_end, max_duration_s, int(max_duration_s*sampling_rate)]
+        # Save gaps so I know to ignore any detections in that region later
+        # Don't save gaps if they are smaller than 5 samples for 100 Hz, 2 samples for 40 Hz
+        gaps = st.get_gaps(min_gap=0.05)
+        # Still need to interpolate the gaps if they are less than min_gap
+        if len(st.get_gaps()) > 0:
+            # Sometimes the gaps have different sampling rates and delta values, not totally sure why. 
+            # The sampling rates do not equal npts/duration for the gap either.
+            try:
+                st.merge(fill_value='interpolate')
+            except:
+                delta = round(1/sampling_rate, 3)
 
-    # Compute duration of gaps at the start and end
-    start_delta = starttime - desired_start
-    end_delta = desired_end - endtime
+                for tr in st:
+                    tr.stats.sampling_rate = sampling_rate
+                    tr.stats.delta = delta
 
-    start_gap = None
-    if start_delta > 1:
-        start_gap = [st[0].stats.network, st[0].stats.station, st[0].stats.location, st[0].stats.channel, 
-                     desired_start, starttime, start_delta, int(start_delta*sampling_rate)]
+                st.merge(fill_value='interpolate')
 
-    end_gap = None
-    if end_delta > 1:
-        end_gap = [st[0].stats.network, st[0].stats.station, st[0].stats.location, st[0].stats.channel,
-                    endtime, desired_end, end_delta, int(end_delta*sampling_rate)]
+        # Make sure the trace is the desired length (fill in ends if needed) TODO: is it possible that the files are too long?
+        # Check for gaps at the start/end of the day and save if they exist
+        start_gap, end_gap = self.format_edge_gaps(st, desired_start, desired_end)
+        if start_gap is not None:
+            gaps.insert(0, start_gap)
+            # Fill the start of the trace with the first value, if needed
+            st = st.trim(starttime=desired_start, pad=True, fill_value=st[0].data[0])
+        if end_gap is not None:
+            gaps += [end_gap]
+            # Fill the end of the trace with the last value, if needed
+            st = st.trim(endtime=desired_end, pad=True, fill_value=st[0].data[-1])
 
-    return start_gap, end_gap
+        return st, gaps 
 
+    @staticmethod
+    def format_edge_gaps(st, desired_start, desired_end, entire_file=False):
+        """Checks for gaps greater than 1 second at the start and end of an Obspy Stream.
+        Only looks at the first trace in the stream. *The desired endtime may be off by ~1 second*
+        If a gap exists returns a list in the format of Obspy's get_gaps(), otherwise returns None.
+
+        Args:
+            st (Obspy Stream): stream to check for gaps
+            max_duration_s (int): expected duration of the file in seconds
+            entire_file (boolean, optional): True if the gap is the entire file. Defaults to False.
+
+        Returns:
+            tupple or list: tupple of gap info lists for the beginning and end of the trace or a list for the entire file gap. 
+        """
+        starttime = st[0].stats.starttime
+        endtime = st[0].stats.endtime 
+        sampling_rate = st[0].stats.sampling_rate
+
+        if entire_file:
+            max_duration_s = desired_end - desired_start
+            return [st[0].stats.network, st[0].stats.station, st[0].stats.location, st[0].stats.channel, 
+                        desired_start, desired_end, max_duration_s, int(max_duration_s*sampling_rate)]
+
+        # Compute duration of gaps at the start and end
+        start_delta = starttime - desired_start
+        end_delta = desired_end - endtime
+
+        start_gap = None
+        if start_delta > 1:
+            start_gap = [st[0].stats.network, st[0].stats.station, st[0].stats.location, st[0].stats.channel, 
+                        desired_start, starttime, start_delta, int(start_delta*sampling_rate)]
+
+        end_gap = None
+        if end_delta > 1:
+            end_gap = [st[0].stats.network, st[0].stats.station, st[0].stats.location, st[0].stats.channel,
+                        endtime, desired_end, end_delta, int(end_delta*sampling_rate)]
+
+        return start_gap, end_gap
+
+    @staticmethod
+    def get_n_windows(npts, window_length, sliding_interval):
+        return (npts-window_length)//sliding_interval + 1
--- pytests/test_apply_detectors.py
+++ pytests/test_apply_detectors.py
@@ -5,30 +5,34 @@ import numpy as np
 import obspy
 
 def test_n_windows():
+    dl = apply_detectors.DataLoader()
     npts = 1000
     window_length = 200
     sliding_interval = 100
     # A time-series of length 1000, with a window length of 200 and a sliding interval of 100 
     # should produce 9 windows
-    assert apply_detectors.get_n_windows(npts, window_length, sliding_interval) == 9
+    assert dl.get_n_windows(npts, window_length, sliding_interval) == 9
 
 def test_load_data_different_sampling_rate_issue():
+    dl = apply_detectors.DataLoader()
     # I know this has gaps in it
     file = '/uufs/chpc.utah.edu/common/home/u1072028/PycharmProjects/seis_proc_dl/pytests/WY.YWB..EHZ__2002-01-01T00:00:00.000000Z__2002-01-02T00:00:00.000000Z.mseed'
-    st, _ = apply_detectors.load_channel_data(file, min_signal_percent=0)
+    st, _ = dl.load_channel_data(file, min_signal_percent=0)
     assert len(st.get_gaps()) == 0
 
 def test_load_data_not_enough_signal():
+    dl = apply_detectors.DataLoader()
     # I know this has gaps in it
     file = '/uufs/chpc.utah.edu/common/home/u1072028/PycharmProjects/seis_proc_dl/pytests/WY.YWB..EHZ__2002-01-01T00:00:00.000000Z__2002-01-02T00:00:00.000000Z.mseed'
-    st, gaps = apply_detectors.load_channel_data(file, min_signal_percent=5)
+    st, gaps = dl.load_channel_data(file, min_signal_percent=5)
     assert st == None
     assert len(gaps) == 1
 
 def test_load_data_filling_ends():
+    dl = apply_detectors.DataLoader()
     # I know this has gaps in it
     file = '/uufs/chpc.utah.edu/common/home/u1072028/PycharmProjects/seis_proc_dl/pytests/WY.YWB..EHZ__2002-01-01T00:00:00.000000Z__2002-01-02T00:00:00.000000Z.mseed'
-    st, _ = apply_detectors.load_channel_data(file, min_signal_percent=0)
+    st, _ = dl.load_channel_data(file, min_signal_percent=0)
     # Check the startime
     assert (st[0].stats.starttime - UTC("2002-01-01")) < 1
     # Check the endtime
@@ -37,8 +41,9 @@ def test_load_data_filling_ends():
     assert abs(st[0].stats.npts - 8640000) <= 1
 
 def test_load_data_end_gaps_added():
+    dl = apply_detectors.DataLoader()
     file = '/uufs/chpc.utah.edu/common/home/u1072028/PycharmProjects/seis_proc_dl/pytests/WY.YWB..EHZ__2002-01-01T00:00:00.000000Z__2002-01-02T00:00:00.000000Z.mseed'
-    _, gaps = apply_detectors.load_channel_data(file, min_signal_percent=0)  
+    _, gaps = dl.load_channel_data(file, min_signal_percent=0)  
     # Know there is 1 gap between signal and signals do not go to the end of the hour
     assert len(gaps) == 3 
     assert gaps[0][4] == UTC('2002-1-1')
@@ -63,8 +68,9 @@ def test_load_data_small_gaps():
     tr.trim(starttime=t+0.05)
     st += tr
     st.write(file, format="MSEED")
-    
-    st, gaps = apply_detectors.load_channel_data(file, min_signal_percent=0)
+
+    dl = apply_detectors.DataLoader()
+    st, gaps = dl.load_channel_data(file, min_signal_percent=0)
 
     # There should be no gaps returned and the trace should be continuous
     assert len(gaps) == 0
@@ -72,4 +78,4 @@ def test_load_data_small_gaps():
     assert len(st.get_gaps()) == 0 
 
 if __name__ == '__main__':
-   test_load_data_small_gaps()
+   test_load_data_filling_ends()
