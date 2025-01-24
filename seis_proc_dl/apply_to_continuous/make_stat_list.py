e23e71afc12de1aec64bb8e6c9fc02fe8cbc9ad8
aarmstrong06 <aa.armstrong6@gmail.com>
2025-01-24 11:21:17 -0700

    update station list name

--- seis_proc_dl/apply_to_continuous/make_stat_list.py
+++ seis_proc_dl/apply_to_continuous/make_stat_list.py
@@ -8,7 +8,7 @@ argParser.add_argument("--ddir", type=str, help="path to data directory",
                        default="/uufs/chpc.utah.edu/common/home/koper-group3/alysha/ys_data/downloaded_all_data")
 argParser.add_argument("-y", "--year", type=str, help="year to gather stations")
 argParser.add_argument("-c", "--ncomps", type=int, help="number of components")
-argParser.add_argument("-s", "--stattype", type=str, help="first letter(s) of the channel code", default="[HEB]")
+argParser.add_argument("-s", "--stattype", type=str, help="first letter(s) of the channel code", default="HEB")
 argParser.add_argument("--outdir", type=str, help="output directory", default=None)
 argParser.add_argument("-f", "--outfile", type=str, help="output filename", default=None)
 argParser.add_argument("-n", "--nstats", type=int, help="limit on the number of stations", default=None)
@@ -22,13 +22,10 @@ outfile = args.outfile
 ddir = args.ddir
 nstats = args.nstats
 
-if stat_type is None:
-    stat_type = "[HEB]"
-
 if outfile is None and nstats is None:
     outfile = f"station.list.{year}.{ncomps}C.{stat_type}H.txt"
 elif nstats is not None:
-    outfile = f"station.list.{year}.{ncomps}C.{stat_type}H.{nstats}stats.txt"
+    outfile = f"station.list.{year}.{ncomps}C.{stat_type}.{nstats}stats.txt"
 
 if outdir is not None:
     outfile = os.path.join(outdir, outfile)
@@ -37,7 +34,7 @@ assert ncomps in [1, 3], "Invalid number of components"
 
 inv = obspy.read_inventory(os.path.join(ddir, str(year), "stations/*"))
 
-chan_list =  [chan[:-1] for chan in inv.select(channel=f"{stat_type}??").get_contents()['channels']]
+chan_list =  [chan[:-1] for chan in inv.select(channel=f"[{stat_type}]??").get_contents()['channels']]
 
 uniq_chans, chan_cnts = np.unique(chan_list, return_counts=True)
 
